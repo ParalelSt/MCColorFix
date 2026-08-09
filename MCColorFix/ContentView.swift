917069e6b899560ce68baba934bb1c5abb2dc60c
Steven <programmerstevie@gmail.com>
2026-08-09 07:27:59 +0700

    Initial Commit

---
+++ MCColorFix/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MCColorFix
+//
+//  Created by Steven on 25/02/1448 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MCColorFix/MCColorFixApp.swift
@@ -0,0 +1,17 @@
+//
+//  MCColorFixApp.swift
+//  MCColorFix
+//
+//  Created by Steven on 25/02/1448 AH.
+//
+
+import SwiftUI
+
+@main
+struct MCColorFixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
